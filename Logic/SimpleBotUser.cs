edbe1bf8a159c6782f27cd91df51e2c6ac3a21e0
Alceu Pereira <alceu.pereira@yahoo.com.br>
2018-02-24 09:58:15 -0300

    teste de comentario

--- Logic/SimpleBotUser.cs
+++ Logic/SimpleBotUser.cs
@@ -9,7 +9,8 @@ namespace SimpleBot
     {
         public static string Reply(Message message)
         {
-            return $"{message.User} disse '{message.Text}'";
+            //return $"{message.User} disse '{message.Text}'";
+              return $"{message.User} disse '{message.Text}'";
         }
 
         public static UserProfile GetProfile(string id)
